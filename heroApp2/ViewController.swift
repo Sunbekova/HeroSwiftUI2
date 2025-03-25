1d2cf8fc5d8fa48154dabbde4a77056341d3f1fc
Sunbekova <suanbekova05@gmail.com>
2025-03-25 22:39:28 +0500

    Initial Commit

---
+++ heroApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  heroApp2
+//
+//  Created by Aisha Suanbekova Bakytjankyzy on 25.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
